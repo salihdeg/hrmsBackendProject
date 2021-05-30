e05e302c311d2b54ea13d3de270ea308a5a91935
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-05-30 18:18:29 +0300

    Result Support Added

---
+++ hrms/src/main/java/kodlamaio/hrms/core/utilities/results/SuccessDataResult.java
@@ -0,0 +1,13 @@
+package kodlamaio.hrms.core.utilities.results;
+
+public class SuccessDataResult<T> extends DataResult<T> {
+
+	public SuccessDataResult(T data) {
+		super(data, true);
+	}
+
+	public SuccessDataResult(T data, String message) {
+		super(data, true, message);
+	}
+
+}
