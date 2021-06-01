ab13c5164e11fe412dbc288390b77f06fc41bd47
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-06-01 15:57:18 +0300

    Cities Added

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/City.java
@@ -0,0 +1,41 @@
+package kodlamaio.hrms.entities.concretes;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cities")
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler", "jobAdvertisements" })
+public class City {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "code")
+	private char[] code;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@OneToMany(mappedBy = "city")
+	private List<JobAdvertisement> jobAdvertisements;
+	
+}
