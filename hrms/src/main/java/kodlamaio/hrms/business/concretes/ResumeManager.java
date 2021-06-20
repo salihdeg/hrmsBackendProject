2597fd753d54f083bfd39aa8ca3d72f28ef0f3ae
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-06-21 00:34:36 +0300

    Property Checker Added

--- hrms/src/main/java/kodlamaio/hrms/business/concretes/ResumeManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/ResumeManager.java
@@ -14,8 +14,8 @@ import kodlamaio.hrms.dataAccess.abstracts.ResumeDao;
 import kodlamaio.hrms.entities.concretes.Resume;
 
 @Service
-public class ResumeManager implements ResumeService{
-	
+public class ResumeManager implements ResumeService {
+
 	private ResumeDao resumeDao;
 
 	@Autowired
@@ -26,6 +26,7 @@ public class ResumeManager implements ResumeService{
 
 	@Override
 	public Result add(Resume resume) {
+		this.checkAndSetFieldsForResume(resume);
 		this.resumeDao.save(resume);
 		return new SuccessResult("Resume added");
 	}
@@ -34,5 +35,71 @@ public class ResumeManager implements ResumeService{
 	public DataResult<List<Resume>> getAll() {
 		var result = this.resumeDao.findAll();
 		return new SuccessDataResult<List<Resume>>(result, "All Resumes Listed");
+	}
+
+	private void setEductaionInfosIfExist(Resume resume) {
+		if (!resume.getEducationInfos().isEmpty()) {
+			for (var educationInfos : resume.getEducationInfos()) {
+				educationInfos.setResume(resume);
+			}
+		}
+	}
+
+	private void setExperiencesIfExist(Resume resume) {
+		if (!resume.getExperiences().isEmpty()) {
+			for (var experiences : resume.getExperiences()) {
+				experiences.setResume(resume);
+			}
+		}
+	}
+
+	private void setWorkerSkillsIfExist(Resume resume) {
+		if (!resume.getWorkerSkills().isEmpty()) {
+			for (var workerSkills : resume.getWorkerSkills()) {
+				workerSkills.setResume(resume);
+			}
+		}
+	}
+
+	private void setKnownLanguagesIfExist(Resume resume) {
+		if (!resume.getKnownLanguages().isEmpty()) {
+			for (var knownLanguages : resume.getKnownLanguages()) {
+				knownLanguages.setResume(resume);
+			}
+		}
+	}
+
+	private void setSocialAccountsIfExist(Resume resume) {
+		if (!resume.getSocialAccounts().isEmpty()) {
+			for (var socialAccounts : resume.getSocialAccounts()) {
+				socialAccounts.setResume(resume);
+			}
+		}
+	}
+
+	private void setWorkerPicturesIfExist(Resume resume) {
+		if (!resume.getWorkerPictures().isEmpty()) {
+			for (var workerPictures : resume.getWorkerPictures()) {
+				workerPictures.setResume(resume);
+			}
+		}
+	}
+
+	private void setCoverLettersIfExist(Resume resume) {
+		if (!resume.getCoverLetters().isEmpty()) {
+			for (var coverLetters : resume.getCoverLetters()) {
+				coverLetters.setResume(resume);
+			}
+		}
+	}
+
+	private void checkAndSetFieldsForResume(Resume resume) {
+		this.setCoverLettersIfExist(resume);
+		this.setEductaionInfosIfExist(resume);
+		this.setExperiencesIfExist(resume);
+		this.setKnownLanguagesIfExist(resume);
+		this.setSocialAccountsIfExist(resume);
+		this.setWorkerPicturesIfExist(resume);
+		this.setWorkerSkillsIfExist(resume);
 	}
 }
