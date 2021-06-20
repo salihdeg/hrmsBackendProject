2376fd36e2132564e6c9ac4ed1dda199390fcbee
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-06-21 00:36:34 +0300

    Edited for trials

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/WorkerPicturesController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/WorkerPicturesController.java
@@ -9,12 +9,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import kodlamaio.hrms.business.abstracts.WorkerPictureService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.entities.concretes.Resume;
 import kodlamaio.hrms.entities.concretes.WorkerPicture;
 
 @RestController
@@ -34,8 +37,17 @@ public class WorkerPicturesController {
 		return this.workerPictureService.getAll();
 	}
 
-	@PostMapping("/add")
-	public Result add(@Valid @RequestBody WorkerPicture workerPicture, MultipartFile image) throws Exception {
+	@PostMapping(value="/add", consumes = {"multipart/form-data"})
+	public Result add(@RequestBody MultipartFile image, @Valid @RequestBody WorkerPicture workerPicture) throws Exception {
+		return this.workerPictureService.add(image, workerPicture);
+	}
+	
+	@PostMapping(value="/add2", consumes = {"multipart/form-data"})
+	public Result add2(@RequestBody MultipartFile image, @RequestParam int resumeId) throws Exception {
+		WorkerPicture workerPicture = new WorkerPicture();
+		Resume resume = new Resume();
+		resume.setId(resumeId);
+		workerPicture.setResume(resume);
 		return this.workerPictureService.add(image, workerPicture);
 	}
 
